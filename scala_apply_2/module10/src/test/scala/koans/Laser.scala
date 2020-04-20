887a73f59b6416da895a1dafeb35cc7cde8370b6
AlexTolstikhin <alex@ascendify.com>
2020-04-19 21:21:16 -0700

    finished packages

--- scala_apply_2/module10/src/test/scala/koans/Laser.scala
+++ scala_apply_2/module10/src/test/scala/koans/Laser.scala
@@ -1,3 +1,10 @@
 
 /* Copyright (C) 2010-2018 Escalate Software, LLC. All rights reserved. */
+package koans
 
+package laser {
+  class Gun(wattage: Int) {
+    def shoot(): Beam = new Beam(lumens = wattage * 10)
+    class Beam (val lumens: Int){}
+  }
+}
--- scala_apply_2/module10/src/test/scala/koans/Module10.scala
+++ scala_apply_2/module10/src/test/scala/koans/Module10.scala
@@ -14,26 +14,30 @@ class Module10 extends FunSuite with Matchers with SeveredStackTraces {
     // it uses, but don't change the test, import the right package or class(es) instead
     // you should be able to fix each with just one import source line
 
-    // java.sql.Date
-    /* sql.Date.valueOf("2010-8-12").toString should be ("2010-08-12") */
+    java.sql.Date.valueOf("2010-8-12").toString should be ("2010-08-12")
 
     // java.io.File and java.io.FileReader (don't forget the exception as well - in the same package)
-    /* intercept[FileNotFoundException] {
+    import java.io.{File, FileReader, FileNotFoundException}
+
+    intercept[FileNotFoundException] {
       (new FileReader(new File("Hello.txt")))
-    } */
+    }
 
     // Figure this one out for yourself
-    /* List(1,2,3) should be (List(1,2,3)) */
+    List(1,2,3) should be (List(1,2,3))
     // (OK, so it's already imported in package scala.)
 
     // rename java.lang.Math to M
-    /* M.PI should be (3.14159 +- 0.00001) */
+    import java.lang.{Math => M}
+    M.PI should be (3.14159 +- 0.00001)
 
     // scala.collection.immutable.ListSet
-    /* (new immutable.ListSet[String]).isEmpty should be (true) */
+    import scala.collection._
+    (new immutable.ListSet[String]).isEmpty should be (true)
 
     // scala.collection.mutable.HashMap
-    /* (new mutable.HashMap[String, Int]).put("One", 1) should be (None) */ // put returns the old value, if any
+
+    (new mutable.HashMap[String, Int]).put("One", 1) should be (None)// put returns the old value, if any
 
     // the previous two tests can actually be satisfied with just one import in a couple of different ways
     // so remove the import from the last test, and try some options with the previous import to make it
@@ -46,20 +50,20 @@ class Module10 extends FunSuite with Matchers with SeveredStackTraces {
     // equals or hashcode methods.
 
     class Complex(r: Int, i: Int) {
-      private[this] val rl = r
-      private[this] val im = i
+      private val rl = r
+      private val im = i
 
       override def toString = "" + rl + " + " + im + "i"
 
       // uncomment out the line in the equals, make it work without changing the line, or the test
-      override def equals(other: Any) = {
+      override def equals(other: Any):Boolean = {
         other match {
-          // case c: Complex => (rl == c.rl) && (im == c.im)
+          case c: Complex => (rl == c.rl) && (im == c.im)
           case _ => false
         }
       }
 
-      override def hashCode() = this.rl * this.im
+      override def hashCode(): Int = this.rl * this.im
     }
 
     val c1 = new Complex(1, 2)
@@ -75,11 +79,14 @@ class Module10 extends FunSuite with Matchers with SeveredStackTraces {
 
     class ExerciseWheel {
       private def turn() = "Squeak, squeak, squeak"
-
+      class humster {
+        def run(): String = turn()
+      }
+      val hammy = new humster
     }
 
     val wheel = new ExerciseWheel
-    /* wheel.hammy.run should be ("Squeak, squeak, squeak") */
+    wheel.hammy.run should be ("Squeak, squeak, squeak")
   }
 
   test("Shoot a LASER beam") {
@@ -95,7 +102,7 @@ class Module10 extends FunSuite with Matchers with SeveredStackTraces {
     // is called, should return a Gun.Beam with a value "lumens" that is calculated as 10 times the
     // wattage.
 
-    /* import _root_.lab.awesome.laser
+    import _root_.koans._
 
     val highPoweredLaser = new laser.Gun(100)
     val lowPoweredLaser = new laser.Gun(10)
